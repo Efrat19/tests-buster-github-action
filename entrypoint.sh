0c5af7d50af733b2ad9fafc4c8c5f260fbbc8222
Efrat Levitan <41479945+Efrat19@users.noreply.github.com>
2020-03-06 13:00:53 +0200

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,5 +1,29 @@
-#!/bin/sh -l
+#!/bin/sh
+set -e
 
-echo "Hello $1"
-time=$(date)
-echo ::set-output name=time::$time
+function usesBoolean() {
+  [ ! -z "${1}" ] && [ "${1}" = "true" ]
+}
+
+if usesBoolean "${INPUT_HELP}"; then
+  tests-buster bust --help
+  exit 0
+fi
+
+if usesBoolean "${INPUT_VERSION}"; then
+  tests-buster bust --version
+  exit 0
+fi
+
+DRY_RUN=""
+AUTO_REMOVE=""
+
+if usesBoolean "${INPUT_DRY_RUN}"; then
+  DRY_RUN="--dry-run"
+fi
+
+if usesBoolean "${INPUT_AUTO_REMOVE}"; then
+  DRY_RUN="--auto-remove"
+fi
+
+tests-buster bust ${DRY_RUN} ${AUTO_REMOVE} --path ${INPUT_PATH} --pattern ${INPUT_PATTERN}
